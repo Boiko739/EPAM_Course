8c1f30b8fbfc182df805773e359782c2ab8c5896
Boiko739 <boiko.ie739@gmail.com>
2023-08-18 12:45:54 +0300

    Fancy Calc 2
    A practical coding assignment, that was hosted by the AutoCode verification platform

---
+++ fancy-calc6/FancyCalc/Operations.cs
@@ -0,0 +1,25 @@
+﻿namespace FancyCalc
+{
+    public static class Operations
+    {
+        public static int Plus(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Minus(int x, int y)
+        {
+            return x - y;
+        }
+
+        public static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        public static int Sum(int x1, int x2, int x3)
+        {
+            return x1 + x2 + x3;
+        }
+    }
+}
