d1261c5c1ab1c223095a15059165512f51a15ca6
Boiko739 <boiko.ie739@gmail.com>
2023-08-25 18:40:45 +0300

    leap-year created

---
+++ leap-year/LeapYear/Year.cs
@@ -0,0 +1,36 @@
+﻿namespace LeapYearTask
+{
+    public static class Year
+    {
+        /// <summary>
+        /// Returns true if a specified <paramref name="year"/> is a leap year.
+        /// </summary>
+        /// <param name="year">A year to test.</param>
+        /// <returns>True if a specified <paramref name="year"/> is a leap year; otherwise false.</returns>
+        public static bool IsLeapYear(int year)
+        {
+            if (year % 4 == 0)
+            {
+                if (year % 100 == 0)
+                {
+                    if (year % 400 == 0)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
