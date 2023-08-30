9d2567d09d2fea72428dae7bc4de8d5be8c7a143
Boiko739 <boiko.ie739@gmail.com>
2023-08-30 11:32:42 +0300

    population created

---
+++ population/Population/Population.cs
@@ -0,0 +1,57 @@
+﻿namespace PopulationTask
+{
+    public static class Population
+    {
+        /// <summary>
+        /// Calculates the count of years which the town need to see its population greater or equal to currentPopulation inhabitants.
+        /// </summary>
+        /// <param name="initialPopulation">The population at the beginning of a year.</param>
+        /// <param name="percent">The percentage of growth per year.</param>
+        /// <param name="visitors">The visitors (new inhabitants per year) who come to live in the town.</param>
+        /// <param name="currentPopulation">The population at present.</param>
+        /// <returns>The count of years which the town need to see its population greater or equal to currentPopulation inhabitants.</returns>
+        /// <exception cref="ArgumentException">
+        /// Thrown when initial population is less or equals 0
+        /// - or -
+        /// the count of visitors cannot be less 0
+        /// - or -
+        /// the current population is less or equals 0
+        /// - or -
+        /// the current population is less than initial population.
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">Throw if the value of percents is less then 0% or more then 100%.</exception>
+        public static int GetYears(int initialPopulation, double percent, int visitors, int currentPopulation)
+        {
+            if (initialPopulation <= 0)
+            {
+                throw new ArgumentException("The initial population cannot be less or equals 0.", nameof(initialPopulation));
+            }
+            else if (visitors < 0)
+            {
+                throw new ArgumentException("The count of visitors cannot be less than 0.", nameof(initialPopulation));
+            }
+            else if (currentPopulation <= 0)
+            {
+                throw new ArgumentException("The current population cannot be less or equals 0.", nameof(initialPopulation));
+            }
+            else if (currentPopulation < initialPopulation)
+            {
+                throw new ArgumentException("The current population cannot be less than the initial population.", nameof(initialPopulation));
+            }
+            else if (percent < 0 || percent > 100)
+            {
+                throw new ArgumentOutOfRangeException(nameof(percent), "The value of percents cannot be less then 0 or more then 100.");
+            }
+
+            int years = 0;
+            double population = initialPopulation;
+            for (; population < currentPopulation; years++)
+            {
+                population += population * (percent / 100);
+                population += visitors;
+            }
+
+            return years;
+        }
+    }
+}
