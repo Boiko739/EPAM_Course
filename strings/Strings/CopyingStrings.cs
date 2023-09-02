7a4ca6267348cea82c6a7891881f55c252ab9a98
Boiko739 <boiko.ie739@gmail.com>
2023-09-02 13:46:27 +0300

    strings created
    There was a lot of code to change)

---
+++ strings/Strings/CopyingStrings.cs
@@ -0,0 +1,59 @@
+﻿namespace Strings
+{
+    public static class CopyingStrings
+    {
+        /// <summary>
+        /// Copies one character from the <paramref name="source"/> to the <paramref name="destination"/>.
+        /// </summary>
+        public static string CopyOneChar(string source, string destination)
+        {
+            var res = destination.ToCharArray();
+            source.CopyTo(0, res, res.Length - 1, 1);
+            return new string(res);
+        }
+
+        /// <summary>
+        /// Copies three characters from the <paramref name="source"/> to the <paramref name="destination"/>.
+        /// </summary>
+        public static string CopyThreeChars(string source, string destination)
+        {
+            var res = destination.ToCharArray();
+            source.CopyTo(0, res, 0, 3);
+            return new string(res);
+        }
+
+        /// <summary>
+        /// Copies five characters from the <paramref name="source"/> to the <paramref name="destination"/>.
+        /// </summary>
+        public static string CopyFiveChars(string source, string destination)
+        {
+            var res = destination.ToCharArray();
+            source.CopyTo(0, res, 4, 5);
+            return new string(res);
+        }
+
+        /// <summary>
+        /// Copies six characters from the <paramref name="source"/> to the <paramref name="destination"/>.
+        /// </summary>
+        public static string CopySixChars(string source, string destination)
+        {
+            var res = destination.ToCharArray();
+            source.CopyTo(2, res, 5, 6);
+            return new string(res);
+        }
+
+        /// <summary>
+        /// Gets a production code by copying substrings of the <paramref name="regionCode"/>, <paramref name="locationCode"/>, <paramref name="dateCode"/> and <paramref name="factoryCode"/> parameters to the <paramref name="template"/>.
+        /// </summary>
+        public static string GetProductionCode(string template, string regionCode, string locationCode, string dateCode, string factoryCode)
+        {
+            // Sorry for magic numbers
+            var res = template.ToCharArray();
+            regionCode.CopyTo(1, res, 0, 1);
+            locationCode.CopyTo(4, res, 3, 2);
+            dateCode.CopyTo(3, res, 7, 3);
+            factoryCode.CopyTo(2, res, 12, 4);
+            return new string(res);
+        }
+    }
+}
---
+++ strings/Strings/Manipulations.cs
@@ -0,0 +1,77 @@
+﻿using System.Globalization;
+
+namespace Strings
+{
+    public static class Manipulations
+    {
+        /// <summary>
+        /// Returns a greeting.
+        /// </summary>
+        public static string GetHelloGreeting1(string name)
+        {
+            return $"Hello, {name}!";
+        }
+
+        /// <summary>
+        /// Returns a greeting.
+        /// </summary>
+        public static string GetGreeting1(string greeting, string name)
+        {
+            return $"{greeting}, {name.ToUpper(CultureInfo.CurrentCulture)}!";
+        }
+
+        /// <summary>
+        /// Returns a greeting.
+        /// </summary>
+        public static string GetHelloGreeting2(string name)
+        {
+            return $"Hello, {name.Trim()}!";
+        }
+
+        /// <summary>
+        /// Returns a greeting.
+        /// </summary>
+        public static string GetGreeting2(string greeting, string name)
+        {
+            return $"{greeting.Trim()}, {name.Trim().ToLower(CultureInfo.CurrentCulture)}!";
+        }
+
+        /// <summary>
+        /// Returns a greeting.
+        /// </summary>
+        public static string GetHelloGreeting3(string template, string name)
+        {
+            return template.Replace("{name}", name, StringComparison.OrdinalIgnoreCase);
+        }
+
+        /// <summary>
+        /// Returns a greeting.
+        /// </summary>
+        public static string GetGreeting3(string template, string greeting, string name)
+        {
+            return template.Replace("{greeting}", greeting.ToLower(CultureInfo.CurrentCulture), StringComparison.OrdinalIgnoreCase)
+                           .Replace("{name}", name.ToUpper(CultureInfo.CurrentCulture), StringComparison.OrdinalIgnoreCase);
+        }
+
+        /// <summary>
+        /// Returns refined code - without zero characters.
+        /// </summary>
+        public static string GetRefinedCode(string code)
+        {
+            return code.Replace("0", string.Empty, StringComparison.Ordinal);
+        }
+
+        /// <summary>
+        /// Returns refined date - without dash characters.
+        /// </summary>
+        public static string GetRefinedDate(string date)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                date = date.Remove(date.IndexOf('-', 0, 5), 1);
+            }
+
+            return date;
+        }
+    }
+}
