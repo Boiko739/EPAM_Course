a67f373355add5966811896bdce9dc7e1e0512af
Boiko739 <boiko.ie739@gmail.com>
2023-09-04 16:36:53 +0300

    counting-string-chars6 created
    It was EZ

---
+++ counting-string-chars6/CountingStringChars/DoWhileMethods.cs
@@ -0,0 +1,117 @@
+﻿namespace CountingStringChars
+{
+    public static class DoWhileMethods
+    {
+        /// <summary>
+        /// Returns a number of digits in a string.
+        /// </summary>
+        /// <param name="str">A <see cref="string"/> to search.</param>
+        /// <returns>A number of digits in a string.</returns>
+        public static int GetDigitCount(string? str)
+        {
+            if (str == null)
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+
+            int digitCount = 0;
+            int i = 0;
+            if (str.Length > 0)
+            {
+                do
+                {
+                    if (char.IsDigit(str[i]))
+                    {
+                        digitCount++;
+                    }
+
+                    i++;
+                }
+                while (i < str.Length);
+            }
+
+            return digitCount;
+        }
+
+        /// <summary>
+        /// Returns a number of letters in a string.
+        /// </summary>
+        /// <param name="str">A <see cref="string"/> to search.</param>
+        /// <returns>A number of letters in a string.</returns>
+        public static int GetLetterCount(string? str)
+        {
+            if (str == null)
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+
+            int letterCount = 0;
+            int i = 0;
+            if (str.Length > 0)
+            {
+                do
+                {
+                    if (char.IsLetter(str[i]))
+                    {
+                        letterCount++;
+                    }
+
+                    i++;
+                }
+                while (i < str.Length);
+            }
+
+            return letterCount;
+        }
+
+        /// <summary>
+        /// Returns a number of digits in a string.
+        /// </summary>
+        /// <param name="str">A <see cref="string"/> to search.</param>
+        /// <returns>A number of digits in a string.</returns>
+        public static int GetDigitCountRecursive(string? str)
+        {
+            if (str is null)
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+
+            return GetDigitCountRecursive(str, str.Length, 0);
+        }
+
+        /// <summary>
+        /// Returns a number of letters in a string.
+        /// </summary>
+        /// <param name="str">A <see cref="string"/> to search.</param>
+        /// <returns>A number of letters in a string.</returns>
+        public static int GetLetterCountRecursive(string? str)
+        {
+            if (str is null)
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+
+            return GetLetterCountRecursive(str, str.Length, 0);
+        }
+
+        private static int GetDigitCountRecursive(string str, int charsLeft, int counter)
+        {
+            if (charsLeft > 0)
+            {
+                return GetDigitCountRecursive(str, charsLeft - 1, char.IsDigit(str[^charsLeft]) ? counter + 1 : counter);
+            }
+
+            return counter;
+        }
+
+        private static int GetLetterCountRecursive(string str, int charsLeft, int counter)
+        {
+            if (charsLeft > 0)
+            {
+                return GetLetterCountRecursive(str, charsLeft - 1, char.IsLetter(str[^charsLeft]) ? counter + 1 : counter);
+            }
+
+            return counter;
+        }
+    }
+}
