50992dd7c4a75a403da9497545297a40df04875c
Boiko739 <boiko.ie739@gmail.com>
2023-12-14 20:38:17 +0200

    Data Processing and sqlcreatetable
    a new chapter in this learning path

---
+++ Data Processing/sqlcreatetable/SqlCreateTable/sql_queries/create.sql
@@ -0,0 +1,64 @@
+
+DROP TABLE IF EXISTS TaskStatusUpdates;
+DROP TABLE IF EXISTS EmployeeTasks;
+DROP TABLE IF EXISTS Tasks;
+DROP TABLE IF EXISTS EmployeeProjects;
+DROP TABLE IF EXISTS Positions;
+DROP TABLE IF EXISTS Projects;
+DROP TABLE IF EXISTS Employees;
+
+CREATE TABLE Employees (
+    EmployeeID INT PRIMARY KEY,
+    FirstName VARCHAR(100) NOT NULL,
+    LastName VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Projects (
+    ProjectID INT PRIMARY KEY,
+    Name VARCHAR(100) NOT NULL,
+    CreationDate DATE NOT NULL,
+    Status VARCHAR(10) CHECK (Status IN ('open', 'closed')),
+    ClosureDate DATE
+);
+
+CREATE TABLE Positions (
+    PositionID INT PRIMARY KEY,
+    Title VARCHAR(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE EmployeeProjects (
+    EmployeeID INT,
+    ProjectID INT,
+    PositionID INT,
+    PRIMARY KEY (EmployeeID, ProjectID),
+    FOREIGN KEY (EmployeeID) REFERENCES Employees(EmployeeID),
+    FOREIGN KEY (ProjectID) REFERENCES Projects(ProjectID),
+    FOREIGN KEY (PositionID) REFERENCES Positions(PositionID)
+);
+
+CREATE TABLE Tasks (
+    TaskID INT PRIMARY KEY,
+    ProjectID INT,
+    Description VARCHAR(255) NOT NULL,
+    Deadline DATE NOT NULL,
+    Status VARCHAR(20) CHECK (Status IN ('open', 'completed', 'requires revision', 'closed')),
+    FOREIGN KEY (ProjectID) REFERENCES Projects(ProjectID)
+);
+
+CREATE TABLE EmployeeTasks (
+    EmployeeID INT,
+    TaskID INT,
+    PRIMARY KEY (EmployeeID, TaskID),
+    FOREIGN KEY (EmployeeID) REFERENCES Employees(EmployeeID),
+    FOREIGN KEY (TaskID) REFERENCES Tasks(TaskID)
+);
+
+CREATE TABLE TaskStatusUpdates (
+    TaskID INT,
+    Status VARCHAR(20) CHECK (Status IN ('open', 'completed', 'requires revision', 'closed')),
+    UpdateDate DATE NOT NULL,
+    UpdatedBy INT,
+    PRIMARY KEY (TaskID, UpdateDate),
+    FOREIGN KEY (TaskID) REFERENCES Tasks(TaskID),
+    FOREIGN KEY (UpdatedBy) REFERENCES Employees(EmployeeID)
+);
