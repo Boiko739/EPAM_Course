698a9a34b3e3f452ea07043e8a60599ed69619ea
Boiko739 <boiko.ie739@gmail.com>
2023-12-22 16:42:52 +0200

    simple-grouping created

---
+++ Data Processing/simple-grouping/SimpleGrouping/sql_queries/task3.sql
@@ -0,0 +1,7 @@
+﻿SELECT SUM((order_details.price - order_details.price_with_discount) * order_details.product_amount) AS max_order_discount
+FROM customer_order
+JOIN order_details ON customer_order.id = order_details.customer_order_id
+WHERE order_details.price_with_discount < order_details.price
+GROUP BY customer_order.id
+ORDER BY max_order_discount DESC
+LIMIT 1;
