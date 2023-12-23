f1810d8bad6c1c6da8d49733e82a9785083ebbb1
Boiko739 <boiko.ie739@gmail.com>
2023-12-23 13:16:22 +0200

    inner-join created

---
+++ Data Processing/inner-join/InnerJoin/sql_queries/task1.sql
@@ -0,0 +1,4 @@
+﻿SELECT p.id AS id, pt.title AS title, p.price AS price
+FROM product as p
+INNER JOIN product_title AS pt ON p.product_title_id = pt.id
+ORDER BY pt.title ASC;
---
+++ Data Processing/inner-join/InnerJoin/sql_queries/task3.sql
@@ -0,0 +1,9 @@
+﻿SELECT DISTINCT pt.title, pr.price
+FROM order_details od
+INNER JOIN product pr ON od.product_id = pr.id
+INNER JOIN product_title pt ON pr.product_title_id = pt.id
+INNER JOIN customer_order co ON od.customer_order_id = co.id
+INNER JOIN customer c ON co.customer_id = c.person_id
+INNER JOIN person pe ON c.person_id = pe.id
+WHERE pe.birth_date BETWEEN '2000-01-01' AND '2010-12-31'
+ORDER BY pt.title ASC, pr.price ASC;
